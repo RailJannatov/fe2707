fc74064f95d95a6415b0954188553b49d2c0592c
RailJannatov <railcennetov@gmail.com>
2024-10-23 11:05:15 +0400

    nullish coealesing

--- JS(BROWSER)/main.js
+++ JS(BROWSER)/main.js
@@ -32,40 +32,102 @@
 
 // console.log(a === c); //false
 
-let user = {
-  _name: "John",
-  _surname: "Smith",
-  _group: "fe201",
+// let user = {
+//   _name: "John",
+//   _surname: "Smith",
+//   _group: "fe201",
 
-  //   get name() {
-  //     return this._name;
-  //   },
+//   //   get name() {
+//   //     return this._name;
+//   //   },
 
-  //   set name(value) {
-  //     this._name = value;
-  //   },
-  get surname() {
-    this._surname;
-  },
-  set surname(value) {
-    if (value > 5) {
-      this._surname = value;
-    } else {
-      alert("salamm");
-    }
-  },
-  get group() {
-    return this._group;
-  },
-  set group(value) {
-    console.log(value);
-    if (value.length < 4) {
-      console.log("herere");
-    } else {
-      [this._group] = value;
-    }
-  },
-};
-console.log(user);
-user.group = "FE201";
-console.log(user._group);
+//   //   set name(value) {
+//   //     this._name = value;
+//   //   },
+//   get surname() {
+//     this._surname;
+//   },
+//   set surname(value) {
+//     if (value > 5) {
+//       this._surname = value;
+//     } else {
+//       alert("salamm");
+//     }
+//   },
+//   get group() {
+//     return this._group;
+//   },
+//   set group(value) {
+//     console.log(value);
+//     if (value.length < 4) {
+//       console.log("herere");
+//     } else {
+//       [this._group] = value;
+//     }
+//   },
+// };
+
+// console.log(user);
+// user.group = "FE201";
+// console.log(user._group);
+
+// let person = {
+//   haveCar: true,
+// };
+
+// console.log(person.test);
+
+let b = 0;
+let c = [];
+// console.log(!!c);
+
+let name = "" || "salamm";
+
+let age = "" ?? 100;
+
+// let babyAge = 0 || 100;
+
+// if (babyAge !== null || babyAge !== undefined) {
+//   babyAge = 0;
+// }
+
+// let babyAge = (1 && 0) ?? 200;
+
+// let anotherAge = ([] || "") ?? 4;
+
+// console.log(babyAge);
+// console.log(anotherAge);
+
+// console.log(name);
+// console.log(age);
+
+// function getFullName() {
+//   let sum = a * b;
+//   debugger;
+//   // console.log({ a });
+//   // console.log(b);
+//   // console.log(sum);
+//   return sum;
+// }
+
+// getFullName();
+// let a = "";
+
+let a = 10;
+
+// if (a) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// a === 2
+//   ? console.log("false")
+//   : a === 4
+//   ? console.log("false")
+//   : a === 8
+//   ? onsole.log("false")
+//   : console.log("true");
+// if (a === 2) {
+//   console.log("false");
+// }
